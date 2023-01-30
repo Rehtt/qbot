14381f6cd854b1102802d85ddd0f49114aa2b18b
Rehtt <dsreshiram@gmail.com>
2023-01-30 16:48:54 +0800

    :beers: 添加命令

--- cqhttp_bot/comment/comment.go
+++ cqhttp_bot/comment/comment.go
@@ -1,13 +1,14 @@
 package comment
 
 import (
+	"fmt"
 	"github.com/Rehtt/qbot/cqhttp_bot"
 	"strings"
 )
 
 type Cmd struct {
 	bot      *cqhttp_bot.Bot
-	comments comments
+	Comments comments
 }
 type comments map[string]*Comment
 type Comment struct {
@@ -23,7 +24,7 @@ type RunFunc func(paramete string, flag Flag, bot *cqhttp_bot.Bot, messageType c
 func New(bot *cqhttp_bot.Bot) (c *Cmd) {
 	c = new(Cmd)
 	c.bot = bot
-	c.comments = DefaultComment(c)
+	c.Comments = DefaultComment(c)
 	c.run()
 	return
 }
@@ -39,7 +40,7 @@ func (c *Cmd) run() {
 func (c *Cmd) parseMessage(messageType cqhttp_bot.EventMessageType, messageId int32, senderQid, groupId int64, message *cqhttp_bot.EventMessage) {
 	for _, m := range message.Messages {
 		if m.Type == cqhttp_bot.TEXT && m.Text[0] == '/' {
-			com, arg, p := c.comments.parse(m.Text[1:])
+			com, arg, p := c.Comments.Parse(m.Text[1:])
 			//com, arg, p := parseComment(m.Text[1:], c.comments, nil)
 			if com != nil {
 				com.Run(p, arg, c.bot, messageType, messageId, senderQid, groupId, message)
@@ -47,29 +48,40 @@ func (c *Cmd) parseMessage(messageType cqhttp_bot.EventMessageType, messageId in
 		}
 	}
 }
-func (c comments) parse(str string) (co *Comment, f Flag, p string) {
+func (c comments) Parse(str string) (co *Comment, f Flag, p string) {
 	strArr := strings.Split(str, " ")
 	var flagName string
 	for i := 0; i < len(strArr); i++ {
 		s := strArr[i]
+		if s == "" {
+			continue
+		}
 		if flagName != "" {
 			f.set(flagName, s)
 			flagName = ""
 			continue
 		}
-		if com, ok := c[s]; ok {
-			co = com
+		if co == nil {
+			if com, ok := c[s]; ok {
+				co = com
+				flagName = ""
+				continue
+			}
 		} else {
-			if co != nil && co.flag != nil && s[0] == '-' {
+			if com, ok := co.sub[s]; ok {
+				co = com
+				flagName = ""
+				continue
+			}
+			if co.flag != nil && s[0] == '-' {
 				flagName = s[1:]
-				if _, ok = co.flag.Get(flagName); ok {
+				if _, ok := co.flag.Get(flagName); ok {
 					cqhttp_bot.DeepCopy(&f.args, co.flag.args)
 					continue
 				}
 			}
 			return co, f, strings.Join(strArr[i:], " ")
 		}
-		flagName = ""
 	}
 	return
 }
@@ -77,11 +89,11 @@ func (c *Cmd) AddComment(comments ...*Comment) {
 	if len(comments) == 0 {
 		return
 	}
-	if c.comments == nil {
-		c.comments = make(map[string]*Comment, len(comments))
+	if c.Comments == nil {
+		c.Comments = make(map[string]*Comment, len(comments))
 	}
 	for _, sub := range comments {
-		c.comments[sub.Name] = sub
+		c.Comments[sub.Name] = sub
 	}
 }
 func (c *Comment) AddSubComment(comments ...*Comment) {
@@ -101,16 +113,34 @@ func (c *Comment) Flag() *Flag {
 	}
 	return c.flag
 }
+func (c *Cmd) GetComment(name string) *Comment {
+	return c.Comments[name]
+}
 
-func (c *Cmd) help() string {
+func (c *Cmd) Help() string {
 	var tmp strings.Builder
-	for _, com := range c.comments {
+	for _, com := range c.Comments {
 		if tmp.Len() != 0 {
 			tmp.WriteString("\n")
 		}
-		tmp.WriteString(com.Name)
-		tmp.WriteString(" ")
-		tmp.WriteString(com.Usage)
+		tmp.WriteString(fmt.Sprintf("/%s %s", com.Name, com.Usage))
+	}
+	return tmp.String()
+}
+func (c *Comment) Help() string {
+	var tmp strings.Builder
+	//tmp.WriteString(fmt.Sprintf("%s %s\n", c.Name, c.Usage))
+	if len(c.sub) != 0 {
+		tmp.WriteString("子命令：\n")
+		for _, sub := range c.sub {
+			tmp.WriteString(fmt.Sprintf(" %s %s\n", sub.Name, sub.Usage))
+		}
+	}
+	if c.flag != nil && len(c.flag.args) != 0 {
+		tmp.WriteString("选项：\n")
+		for _, f := range c.flag.args {
+			tmp.WriteString(fmt.Sprintf(" -%s %s [默认：%s]\n", f.name, f.usage, f.value))
+		}
 	}
 	return tmp.String()
 }
--- cqhttp_bot/comment/default.go
+++ cqhttp_bot/comment/default.go
@@ -1,6 +1,7 @@
 package comment
 
 import (
+	"fmt"
 	"github.com/Rehtt/qbot/cqhttp_bot"
 )
 
@@ -10,11 +11,24 @@ func DefaultComment(c *Cmd) map[string]*Comment {
 			Name:  "help",
 			Usage: "显示帮助",
 			Run: func(paramete string, flag Flag, bot *cqhttp_bot.Bot, messageType cqhttp_bot.EventMessageType, messageId int32, senderQid, groupId int64, message *cqhttp_bot.EventMessage) {
+				var help string
+				if paramete != "" {
+					com, _, _ := c.Comments.Parse(paramete)
+					if com == nil {
+						help = fmt.Sprintf("找不到相关命令 `%s`", paramete)
+					} else {
+						help = fmt.Sprintf("命令：\n%s\n说明：\n%s\n", com.Usage, paramete)
+						help += com.Help()
+					}
+
+				} else {
+					help = c.Help()
+				}
 				switch messageType {
 				case cqhttp_bot.Private:
-					bot.SendPrivateMsg(senderQid, c.help())
+					bot.SendPrivateMsg(senderQid, help)
 				case cqhttp_bot.Group:
-					bot.SendMsg(groupId, cqhttp_bot.MessageArray(cqhttp_bot.TextMessage(c.help())), messageType)
+					bot.SendMsg(groupId, cqhttp_bot.MessageArray(cqhttp_bot.TextMessage(help)), messageType)
 				}
 			},
 		},
