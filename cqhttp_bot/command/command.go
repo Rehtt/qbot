64b0fe5ac8cdf5528852a4596169d68262572e95
Rehtt <dsreshiram@gmail.com>
2023-10-09 09:31:32 +0800

    :art:

--- cqhttp_bot/command/command.go
+++ cqhttp_bot/command/command.go
@@ -2,8 +2,9 @@ package command
 
 import (
 	"fmt"
-	"github.com/Rehtt/qbot/cqhttp_bot"
 	"strings"
+
+	"github.com/Rehtt/qbot/cqhttp_bot"
 )
 
 type Cmd struct {
@@ -11,14 +12,16 @@ type Cmd struct {
 	trigger  string
 	Commands Commands
 }
-type Commands map[string]*Command
-type Command struct {
-	Name  string
-	Usage string
-	flag  *Flag
-	Run   RunFunc
-	sub   Commands
-}
+type (
+	Commands map[string]*Command
+	Command  struct {
+		Name  string
+		Usage string
+		flag  *Flag
+		Run   RunFunc
+		sub   Commands
+	}
+)
 
 type RunFunc func(paramete string, flag Flag, bot *cqhttp_bot.Bot, ctx *cqhttp_bot.EventMessageContext)
 
@@ -34,22 +37,25 @@ func New(bot *cqhttp_bot.Bot) (c *Cmd) {
 func (c *Cmd) Trigger(t string) {
 	c.trigger = t
 }
+
 func (c *Cmd) run() {
 	c.bot.OnMessage(func(ctx *cqhttp_bot.EventMessageContext) {
 		c.parseMessage(ctx)
 	})
 }
+
 func (c *Cmd) parseMessage(ctx *cqhttp_bot.EventMessageContext) {
 	for _, m := range ctx.Message.Messages {
 		if m.Type == cqhttp_bot.TEXT && strings.HasPrefix(m.Text, c.trigger) {
 			com, arg, p := c.Commands.Parse(m.Text[len(c.trigger):])
-			//com, arg, p := parseCommand(m.Text[1:], c.Commands, nil)
+			// com, arg, p := parseCommand(m.Text[1:], c.Commands, nil)
 			if com != nil {
 				com.Run(p, arg, c.bot, ctx)
 			}
 		}
 	}
 }
+
 func (c Commands) Parse(str string) (co *Command, f Flag, p string) {
 	fields := strings.Fields(str)
 	var flagName string
@@ -110,6 +116,7 @@ func (c Commands) Parse(str string) (co *Command, f Flag, p string) {
 	}
 	return
 }
+
 func (c *Cmd) AddCommand(Commands ...*Command) {
 	if len(Commands) == 0 {
 		return
@@ -121,6 +128,7 @@ func (c *Cmd) AddCommand(Commands ...*Command) {
 		c.Commands[sub.Name] = sub
 	}
 }
+
 func (c *Command) AddSubCommand(Commands ...*Command) {
 	if len(Commands) == 0 {
 		return
@@ -132,12 +140,14 @@ func (c *Command) AddSubCommand(Commands ...*Command) {
 		c.sub[sub.Name] = sub
 	}
 }
+
 func (c *Command) Flag() *Flag {
 	if c.flag == nil {
 		c.flag = new(Flag)
 	}
 	return c.flag
 }
+
 func (c *Cmd) GetCommand(name string) *Command {
 	return c.Commands[name]
 }
@@ -148,13 +158,14 @@ func (c *Cmd) Help() string {
 		if tmp.Len() != 0 {
 			tmp.WriteString("\n")
 		}
-		tmp.WriteString(fmt.Sprintf("/%s %s", com.Name, com.Usage))
+		tmp.WriteString(fmt.Sprintf("%s%s %s", c.trigger, com.Name, com.Usage))
 	}
 	return tmp.String()
 }
+
 func (c *Command) Help() string {
 	var tmp strings.Builder
-	//tmp.WriteString(fmt.Sprintf("%s %s\n", c.Name, c.Usage))
+	// tmp.WriteString(fmt.Sprintf("%s %s\n", c.Name, c.Usage))
 	if len(c.sub) != 0 {
 		tmp.WriteString("子命令：\n")
 		for _, sub := range c.sub {
